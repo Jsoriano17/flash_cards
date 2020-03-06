38540c75d68bf1c934c6960932f1f89b0c0738c1
Jeffrey Soriano <jeffsor17@gmail.com>
2020-03-05 23:52:45 -0700

    added delete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const flashTitle = document.getElementById('flash-title')
 const flashInfo = document.getElementById('flash-info')
 const next = document.getElementById('next')
+const deletes = document.getElementById('delete')
 const userInputTitle = document.querySelector('user-input-title')
 const userInputInfo = document.querySelector('user-input-info')
 
@@ -9,7 +10,7 @@ let cards = [
     {title: "Arrow Function", info: "Arrow functions are a new short hand to writing functions. Its important to know that they have a very key important difference from normal function, that difference is the 'this' keyword. 'this' has a different scope in the arrow function."},
     {title: "Let", info: "Let works very similar to var the difference being the scope, let has a local scope"},
     {title: "Const", info: "Const declares variables that will have the same value through out the program"},
-    {title: "Var", info: "Var can declare any variable and has a global scope which means it can be accessed from anywhere"},
+    {title: "Var", info: "Var can declare any variable and has a global scope which means it can be accessed from anywhere"}
 ]
 
 let flashHeader = document.getElementById('flash-header')
@@ -46,6 +47,11 @@ next.addEventListener('click', (i) => {
     } else {
         setCardIndex()
     }
+
+})
+deletes.addEventListener('click', () => {
+  destroy = num 
+  cards.splice(destroy, destroy + 1)
 
 })
 
